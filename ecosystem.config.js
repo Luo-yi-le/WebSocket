7c16f41f9f9e85766fc553becbd75f9b1abee6de
lonely <weinet.lonely@gmail.com>
2019-09-27 16:31:00 +0800

    添加nginx代理服务器和PM2进程管理器

---
+++ ecosystem.config.js
@@ -0,0 +1,55 @@
+module.exports = {
+    apps: [
+        {
+            name: 'Html_Server',
+            script: 'src/server/websocket/htmlServer.js',
+            out_file: "src/assets/pm2_logs/info.log",
+            error_file: "src/assets/pm2_logs/error.log",
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '500M',
+            env: {
+                NODE_ENV: 'development'
+            }
+        },
+        {
+            name: 'ws_Server',
+            script: 'src/server/websocket/wsServer.js',
+            out_file: "src/assets/pm2_logs/ws_Server_info.log",
+            error_file: "src/assets/pm2_logs/ws_Server_error.log",
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '500M',
+            env: {
+                NODE_ENV: 'development'
+            }
+        },
+        {
+            name: 'src_Server',
+            script: 'bin/www.js',
+            args: ["-d", "./resource"],
+            out_file: "src/assets/pm2_logs/src_Server_info.log",
+            error_file: "src/assets/pm2_logs/src_Server_error.log",
+            instances: 1,
+            autorestart: true,
+            watch: false,
+            max_memory_restart: '500M',
+            env: {
+                NODE_ENV: 'development'
+            }
+        }
+    ],
+
+    deploy: {
+        production: {
+            user: 'node',
+            host: '212.83.163.1',
+            ref: 'origin/master',
+            repo: 'git@github.com:repo.git',
+            path: '/var/www/production',
+            'post-deploy': 'npm install && pm2 reload ecosystem.config.js --env production'
+        }
+    }
+};
--- src/server/launch.js
+++ src/server/launch.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 //日志
+
+var https = require('https');
+var http = require('http');
+var util = require('util');
+
 var log = require("../assets/log4js/log4js");
 var Tips = require("../../config/zh-Ch");
 var html = require("./websocket/htmlServer");
@@ -10,24 +15,54 @@ if (html !== undefined && html !== null && ws !== undefined && ws !== null) {
     logger.info(Tips.Setting.Success["208"]);
 }
 
-var express = require('express')
+var express = require('express');
 var app = express();
 //发送请求，获取客户端ip
 app.get('/', function (req, res) {
-    var clientIp = getIp(req)
+    var clientIp = getIp(req);
+    getIpInfo(clientIp, function (err, msg) {
+        // console.log('城市: ' + JSON.stringify(msg.data[0].location));
+        console.log('Address: ' + util.inspect(msg.data[0].location, true, 8));
+    })
     console.log(clientIp)
 })
 //通过req的hearers来获取客户端ip
-var getIp = function(req) {
+var getIp = function (req) {
     var ip = req.headers['x-real-ip'] || req.headers['x-forwarded-for'] || req.connection.remoteAddres || req.socket.remoteAddress || '';
-    if(ip.split(',').length>0){
+    if (ip.split(',').length > 0) {
         ip = ip.split(',')[0];
     }
     return ip;
 };
 // 指定ipv4格式
-var server = app.listen(8089, '0.0.0.0',function () {
+var server = app.listen(8089, '0.0.0.0', function () {
     var host = server.address().address
     var port = server.address().port
     console.log('服务启动...')
-})
+});
+
+var getIpInfo = function (ip, cb) {
+    var url = "https://sp0.baidu.com/8aQDcjqpAAV3otqbppnN2DJv/api.php?query="
+        + ip + "&co=&resource_id=6006&t=1555898284898&ie=utf8&oe=utf8&format=json&tn=baidu";
+    https.get(url, function (res) {
+        var code = res.statusCode;
+        if (code == 200) {
+            res.on('data', function (data) {
+                try {
+                    cb(null, JSON.parse(data));
+                } catch (err) {
+                    cb(err);
+                }
+            });
+        } else {
+            cb({code: code});
+        }
+    }).on('error', function (e) {
+        cb(e);
+    });
+};
+// getIpInfo("60.222.230.50", function(err, msg) {
+//    // console.log('城市: ' + JSON.stringify(msg.data[0].location));
+//     console.log('msg: ' + util.inspect(msg.data[0].location, true, 8));
+// })
+
